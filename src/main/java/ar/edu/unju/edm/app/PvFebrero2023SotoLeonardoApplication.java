8c0bf7d5e1b56911a7e26d26afbb18c0a9cf1e6d
Casa <Casa@DESKTOP-26OO8IB>
2023-02-27 19:55:14 -0300

    Agregacion de la clase Usuario y las dependencias

---
+++ src/main/java/ar/edu/unju/edm/app/PvFebrero2023SotoLeonardoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvFebrero2023SotoLeonardoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvFebrero2023SotoLeonardoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ar/edu/unju/edm/app/model/Usuario.java
@@ -0,0 +1,58 @@
+package ar.edu.unju.edm.app.model;
+
+import java.time.LocalDate;
+
+public class Usuario {
+
+	private Long dni;
+	private String nombre;
+	private String apellido;
+	private LocalDate fechaNacimiento;
+	private String nacionalidad;
+	private String contrasena;
+	private String tipoUsuario;
+	
+	
+	public Long getDni() {
+		return dni;
+	}
+	public void setDni(Long dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+	public String getContrasena() {
+		return contrasena;
+	}
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+	public String getTipoUsuario() {
+		return tipoUsuario;
+	}
+	public void setTipoUsuario(String tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
+}
